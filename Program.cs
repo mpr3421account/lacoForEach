1f5fc5d52bdb445cfc88e516d22b8ae23ef4a954
Marcos Paulo <itsolutions031250@gmail.com>
2022-12-07 15:32:51 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] vect = new string[] { "piranha", "vadia", "baranga" };
+
+for(int i =0; i < vect.Length; i++)
+{
+    Console.WriteLine(vect[i]);
+}
+
+Console.WriteLine("__________________________________________________________________________________");
+
+foreach(string obj in vect)
+{
+    Console.WriteLine(obj);//obj é um apelido, pode ser usado qualquer nome
+}
